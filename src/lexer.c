8025154e3060e007216492edaaa8628773d25d95
Ryan Kistner <azuisleet@gmail.com>
2012-04-12 14:18:10 -0600

    Check for NULL pointer before calling strcasecmp in GetVersFromFPI

--- src/lexer.c
+++ src/lexer.c
@@ -180,7 +180,7 @@ static uint GetVersFromFPI(ctmbstr fpi)
     uint i;
 
     for (i = 0; W3C_Doctypes[i].name; ++i)
-        if (TY_(tmbstrcasecmp)(W3C_Doctypes[i].fpi, fpi) == 0)
+        if (W3C_Doctypes[i].fpi != NULL && TY_(tmbstrcasecmp)(W3C_Doctypes[i].fpi, fpi) == 0)
             return W3C_Doctypes[i].vers;
 
     return 0;

e0a61e9b06932dd3beaecd6ecf8e773070f1a886
Geoff McLane <ubuntu@geffair.info>
2014-08-03 18:50:51 +0200

    add another 'cleaner' of html from google docs

---
+++ src/gdoc.h
@@ -0,0 +1,19 @@
+#ifndef __GDOC_H__
+#define __GDOC_H__
+
+/* gdoc.h -- clean up html exported by Google Docs
+
+  (c) 2012 (W3C) MIT, ERCIM, Keio University
+  See tidy.h for the copyright notice.
+
+  - strip the script element, as the style sheet is a mess
+  - strip class attributes
+  - strip span elements, leaving their content in place
+  - replace <a name=...></a> by id on parent element
+  - strip empty <p> elements
+
+*/
+
+void TY_(CleanGoogleDocument)( TidyDocImpl* doc );
+
+#endif /* __GDOC_H__ */

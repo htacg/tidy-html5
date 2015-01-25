813f1c8a13a5aef91d54aaba8f706b05e4540e91
Geoff McLane <ubuntu@geffair.info>
2015-01-25 21:00:13 +0100

    Issue #56 - long outstanding bug on script tag Added a PCondFlushLine before emitting the script tag Certainly looks better, but need to check for any regression

--- src/pprint.c
+++ src/pprint.c
@@ -1775,6 +1775,8 @@ void PPrintScriptStyle( TidyDocImpl* doc, uint mode, uint indent, Node *node )
     int     contentIndent = -1;
     Bool    xhtmlOut = cfgBool( doc, TidyXhtmlOut );
 
+    PCondFlushLine( doc, indent );  /* Issue #56 - long oustanding bug - flush any existing closing tag */
+
     if ( InsideHead(doc, node) )
       TY_(PFlushLine)( doc, indent );
 

049272ce58d6d9a5c84b8a818ea5d25708fc1539
Jim Derry <balthisar@gmail.com>
2015-02-01 00:03:04 +0800

    Added a clean Xcode project for Mac OS X.

---
+++ build/xcode/xcode-version.h
@@ -0,0 +1,10 @@
+// This file is generated automatically. Do not edit. Edit version.txt instead.
+#ifdef LIBTIDY_VERSION
+#undef LIBTIDY_VERSION
+#endif
+#ifdef RELEASE_DATE
+#undef RELEASE_DATE
+#endif
+#define LIBTIDY_VERSION "4.9.3"
+#define RELEASE_DATE "2015/01/31"
+// Updated on Sat Jan 31 23:26:18 CST 2015

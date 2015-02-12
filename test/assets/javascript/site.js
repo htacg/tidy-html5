99bb2b42a5f7c4bc54ca52c16460dc53a3721dee
Jim Derry <balthisar@gmail.com>
2015-02-12 17:39:48 +0800

    Checkpoint -- add manual header navigation item

--- test/assets/javascript/site.js
+++ test/assets/javascript/site.js
@@ -85,7 +85,7 @@ $(document).ready(function (){
             $(this).on('click', function(event) {
         		event.preventDefault();
                 var target = $(event.target).closest("a");
-                var targetHight =  $(target.attr("href")).offset().top
+                var targetHight =  $(target.attr("href")).offset().top;
             	$('html,body').animate({scrollTop: targetHight - 170}, 800, "easeInOutExpo");
             });
         }

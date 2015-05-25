b8bc88522c9af8fef2ad5c66b34c86622c3c671c
Geoff McLane <ubuntu@geoffair.info>
2015-05-25 16:48:39 +0200

    small fix for indent-with-tabs to have a default xml value

--- src/config.c
+++ src/config.c
@@ -320,7 +320,7 @@ static const TidyOptionImpl option_defs[] =
   { TidySortAttributes,          PP, "sort-attributes",             IN, TidySortAttrNone,ParseSorter,       sorterPicks     },
   { TidyMergeSpans,              MU, "merge-spans",                 IN, TidyAutoState,   ParseAutoBool,     autoBoolPicks   },
   { TidyAnchorAsName,            MU, "anchor-as-name",              BL, yes,             ParseBool,         boolPicks       },
-  { TidyPPrintTabs,              PP, "indent-with-tabs",            BL, no,              ParseTabs,         NULL            }, /* 20150515 - Issue #108 */
+  { TidyPPrintTabs,              PP, "indent-with-tabs",            BL, no,              ParseTabs,         boolPicks       }, /* 20150515 - Issue #108 */
   { N_TIDY_OPTIONS,              XX, NULL,                          XY, 0,               NULL,              NULL            }
 };
 

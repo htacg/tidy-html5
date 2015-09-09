88a656f0293b3a1a457ef2a777117f2da6c66ff3
Geoff R. McLane <ubuntu@geoffair.info>
2015-09-09 13:57:14 +0200

    testone.sh - rename 'tidy5' to 'tidy'

--- test/testone.sh
+++ test/testone.sh
@@ -19,7 +19,7 @@ set +f
 TESTNO=$1
 EXPECTED=$2
 #TIDY=../bin/tidy
-TIDY=../build/cmake/tidy5
+TIDY=../build/cmake/tidy
 if [ ! -f "$TIDY" ]; then
 	echo "$BN: Can NOT locate binary '$TIDY'!"
 	echo "$BN: Fix me with the correct location of the binary to run."

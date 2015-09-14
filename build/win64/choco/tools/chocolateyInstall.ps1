30853a3b9df39a8fbd751663288fe36607bfaf7d
Stefan Scherer <scherer_stefan@icloud.com>
2015-09-14 09:55:44 +0200

    Fix name

--- build/win64/choco/tools/chocolateyInstall.ps1
+++ build/win64/choco/tools/chocolateyInstall.ps1
@@ -2,4 +2,4 @@ $url = 'http://www.htacg.org/binaries/binaries/tidy-5.1.8/tidy-5.1.8-win32.zip'
 $url64bit = 'http://www.htacg.org/binaries/binaries/tidy-5.1.8/tidy-5.1.8-win64.zip'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit"
+Install-ChocolateyZipPackage "tidy-html5" "$url" "$unzipLocation" "$url64bit"

8de67cf4d647ffb97366ea8baea2ccbf272fdace
Geoff McLane <ubuntu@geoffair.info>
2017-03-22 02:45:43 +0100

    Some improvement to 'build-bins.bat' - no bump

--- build/win64/build-bins.bat
+++ build/win64/build-bins.bat
@@ -1,3 +1,16 @@
 @setlocal
+@set TMPLOG=bldlog-p.txt
 
-cmake --build . --config Release --target PACKAGE
+@echo Running: 'cmake --build . --config Release --target PACKAGE' out to %TMPLOG%
+@echo Running: 'cmake --build . --config Release --target PACKAGE' > %TMPLOG%
+cmake --build . --config Release --target PACKAGE >> %TMPLOG%
+@if ERRORLEVEL 1 goto ISERR
+@echo Build package done... see %TMPLOG%
+@endlocal
+@exit /b 0
+
+:ISERR
+@endlocal
+@exit /b 1
+
+@REM eof

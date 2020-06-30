6a7813e64deacd62fc9f39488492531f79a88a38
Jesus De Meyer <taxxodium@gmail.com>
2020-06-30 10:33:26 -0500

    removed: comments

--- Package.swift
+++ Package.swift
@@ -6,18 +6,14 @@ import PackageDescription
 let package = Package(
     name: "tidy-html5",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "tidy-html5",
             targets: ["tidy-html5"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+       
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "tidy-html5",
                 dependencies: [],
                 path: ".",

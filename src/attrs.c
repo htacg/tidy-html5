882ff8fe18bb5fcc55610e8fb8ac2f1ac6c702b2
Caleb Callaway <cqcallaw@brainvitamins.net>
2020-11-25 10:59:33 -0800

    Support extended color names in HTML 5 validation
    Fixes #908

--- src/attrs.c
+++ src/attrs.c
@@ -589,7 +589,144 @@ static const struct _colors colors[] =
     { NULL,      NULL      }
 };
 
-static ctmbstr GetColorCode(ctmbstr name)
+static const struct _colors extended_colors[] =
+{
+    { "aliceblue", "#f0f8ff" },
+    { "antiquewhite", "#faebd7" },
+    { "aquamarine", "#7fffd4" },
+    { "azure", "#f0ffff" },
+    { "beige", "#f5f5dc" },
+    { "bisque", "#ffe4c4" },
+    { "blanchedalmond", "#ffebcd" },
+    { "blueviolet", "#8a2be2" },
+    { "brown", "#a52a2a" },
+    { "burlywood", "#deb887" },
+    { "cadetblue", "#5f9ea0" },
+    { "chartreuse", "#7fff00" },
+    { "chocolate", "#d2691e" },
+    { "coral", "#ff7f50" },
+    { "cornflowerblue", "#6495ed" },
+    { "cornsilk", "#fff8dc" },
+    { "crimson", "#dc143c" },
+    { "cyan", "#00ffff" },
+    { "darkblue", "#00008b" },
+    { "darkcyan", "#008b8b" },
+    { "darkgoldenrod", "#b8860b" },
+    { "darkgray", "#a9a9a9" },
+    { "darkgreen", "#006400" },
+    { "darkgrey", "#a9a9a9" },
+    { "darkkhaki", "#bdb76b" },
+    { "darkmagenta", "#8b008b" },
+    { "darkolivegreen", "#556b2f" },
+    { "darkorange", "#ff8c00" },
+    { "darkorchid", "#9932cc" },
+    { "darkred", "#8b0000" },
+    { "darksalmon", "#e9967a" },
+    { "darkseagreen", "#8fbc8f" },
+    { "darkslateblue", "#483d8b" },
+    { "darkslategray", "#2f4f4f" },
+    { "darkslategrey", "#2f4f4f" },
+    { "darkturquoise", "#00ced1" },
+    { "darkviolet", "#9400d3" },
+    { "deeppink", "#ff1493" },
+    { "deepskyblue", "#00bfff" },
+    { "dimgray", "#696969" },
+    { "dimgrey", "#696969" },
+    { "dodgerblue", "#1e90ff" },
+    { "firebrick", "#b22222" },
+    { "floralwhite", "#fffaf0" },
+    { "forestgreen", "#228b22" },
+    { "gainsboro", "#dcdcdc" },
+    { "ghostwhite", "#f8f8ff" },
+    { "gold", "#ffd700" },
+    { "goldenrod", "#daa520" },
+    { "greenyellow", "#adff2f" },
+    { "grey", "#808080" },
+    { "honeydew", "#f0fff0" },
+    { "hotpink", "#ff69b4" },
+    { "indianred", "#cd5c5c" },
+    { "indigo", "#4b0082" },
+    { "ivory", "#fffff0" },
+    { "khaki", "#f0e68c" },
+    { "lavender", "#e6e6fa" },
+    { "lavenderblush", "#fff0f5" },
+    { "lawngreen", "#7cfc00" },
+    { "lemonchiffon", "#fffacd" },
+    { "lightblue", "#add8e6" },
+    { "lightcoral", "#f08080" },
+    { "lightcyan", "#e0ffff" },
+    { "lightgoldenrodyellow", "#fafad2" },
+    { "lightgray", "#d3d3d3" },
+    { "lightgreen", "#90ee90" },
+    { "lightgrey", "#d3d3d3" },
+    { "lightpink", "#ffb6c1" },
+    { "lightsalmon", "#ffa07a" },
+    { "lightseagreen", "#20b2aa" },
+    { "lightskyblue", "#87cefa" },
+    { "lightslategray", "#778899" },
+    { "lightslategrey", "#778899" },
+    { "lightsteelblue", "#b0c4de" },
+    { "lightyellow", "#ffffe0" },
+    { "limegreen", "#32cd32" },
+    { "linen", "#faf0e6" },
+    { "magenta", "#ff00ff" },
+    { "mediumaquamarine", "#66cdaa" },
+    { "mediumblue", "#0000cd" },
+    { "mediumorchid", "#ba55d3" },
+    { "mediumpurple", "#9370db" },
+    { "mediumseagreen", "#3cb371" },
+    { "mediumslateblue", "#7b68ee" },
+    { "mediumspringgreen", "#00fa9a" },
+    { "mediumturquoise", "#48d1cc" },
+    { "mediumvioletred", "#c71585" },
+    { "midnightblue", "#191970" },
+    { "mintcream", "#f5fffa" },
+    { "mistyrose", "#ffe4e1" },
+    { "moccasin", "#ffe4b5" },
+    { "navajowhite", "#ffdead" },
+    { "oldlace", "#fdf5e6" },
+    { "olivedrab", "#6b8e23" },
+    { "orange", "#ffa500" },
+    { "orangered", "#ff4500" },
+    { "orchid", "#da70d6" },
+    { "palegoldenrod", "#eee8aa" },
+    { "palegreen", "#98fb98" },
+    { "paleturquoise", "#afeeee" },
+    { "palevioletred", "#db7093" },
+    { "papayawhip", "#ffefd5" },
+    { "peachpuff", "#ffdab9" },
+    { "peru", "#cd853f" },
+    { "pink", "#ffc0cb" },
+    { "plum", "#dda0dd" },
+    { "powderblue", "#b0e0e6" },
+    { "rebeccapurple", "#663399" },
+    { "rosybrown", "#bc8f8f" },
+    { "royalblue", "#4169e1" },
+    { "saddlebrown", "#8b4513" },
+    { "salmon", "#fa8072" },
+    { "sandybrown", "#f4a460" },
+    { "seagreen", "#2e8b57" },
+    { "seashell", "#fff5ee" },
+    { "sienna", "#a0522d" },
+    { "skyblue", "#87ceeb" },
+    { "slateblue", "#6a5acd" },
+    { "slategray", "#708090" },
+    { "slategrey", "#708090" },
+    { "snow", "#fffafa" },
+    { "springgreen", "#00ff7f" },
+    { "steelblue", "#4682b4" },
+    { "tan", "#d2b48c" },
+    { "thistle", "#d8bfd8" },
+    { "tomato", "#ff6347" },
+    { "turquoise", "#40e0d0" },
+    { "violet", "#ee82ee" },
+    { "wheat", "#f5deb3" },
+    { "whitesmoke", "#f5f5f5" },
+    { "yellowgreen", "#9acd32" },
+    { NULL, NULL }
+};
+
+static ctmbstr GetColorCode(ctmbstr name, Bool use_css_colors)
 {
     uint i;
 
@@ -597,16 +734,26 @@ static ctmbstr GetColorCode(ctmbstr name)
         if (TY_(tmbstrcasecmp)(name, colors[i].name) == 0)
             return colors[i].hex;
 
+    if (use_css_colors)
+        for (i = 0; extended_colors[i].name; ++i)
+            if (TY_(tmbstrcasecmp)(name, extended_colors[i].name) == 0)
+                return extended_colors[i].hex;
+
     return NULL;
 }
 
-static ctmbstr GetColorName(ctmbstr code)
+static ctmbstr GetColorName(ctmbstr code, Bool use_css_colors)
 {
     uint i;
 
     for (i = 0; colors[i].name; ++i)
         if (TY_(tmbstrcasecmp)(code, colors[i].hex) == 0)
             return colors[i].name;
+
+    if (use_css_colors)
+        for (i = 0; extended_colors[i].name; ++i)
+            if (TY_(tmbstrcasecmp)(code, extended_colors[i].hex) == 0)
+                return extended_colors[i].name;
 
     return NULL;
 }
@@ -2020,7 +2167,7 @@ void CheckColor( TidyDocImpl* doc, Node *node, AttVal *attval)
 
     if (valid && given[0] == '#' && cfgBool(doc, TidyReplaceColor))
     {
-        ctmbstr newName = GetColorName(given);
+        ctmbstr newName = GetColorName(given, TY_(IsHTML5Mode)(doc));
 
         if (newName)
         {
@@ -2031,7 +2178,7 @@ void CheckColor( TidyDocImpl* doc, Node *node, AttVal *attval)
 
     /* if it is not a valid color code, it is a color name */
     if (!valid)
-        valid = GetColorCode(given) != NULL;
+        valid = GetColorCode(given, TY_(IsHTML5Mode)(doc)) != NULL;
 
     if (valid && given[0] == '#')
         attval->value = TY_(tmbstrtoupper)(attval->value);
